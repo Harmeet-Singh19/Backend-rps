dc553572f31199ff7fb724662604138ce16ca236
Harmeet <savgun2001@gmail.com>
2020-10-07 20:36:14 +0530

    First commit

---
+++ public/main.js
@@ -0,0 +1,26 @@
+let model, webcam, labelContainer, maxPredictions;
+
+async function init() {
+    //load the model 
+    model = await tf.loadLayersModel('jsweights/model.json')
+
+    console.log("done")
+}
+
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+        labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+}
+
