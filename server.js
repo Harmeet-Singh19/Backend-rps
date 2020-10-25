ae34382b99735a6f78799f3fbb373ce08e627e68
adityakarn <adityakarn26@gmail.com>
2020-10-25 17:20:33 +0530

    will add loader later

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-
 const express = require('express')
 const app = express()
-// const cors = require('cors')
-// app.use(cors())
+    // const cors = require('cors')
+    // app.use(cors())
 const server = require('http').Server(app)
 
 const io = require('socket.io')(server)
@@ -13,66 +12,65 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('homepagetemp')
+    res.render('homepagetemp')
 })
-app.get('/game',(req,res)=>{
-  res.redirect(`/game/${uuidV4()}`)
+app.get('/game', (req, res) => {
+    res.redirect(`/game/${uuidV4()}`)
 })
 app.get('/game/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+    res.render('room', { roomId: req.params.room })
 })
 
 io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    var numClients
-    
-    io.of('/').in(roomId).clients(function(error,clients){
-       numClients=clients.length;
-     // console.log(numClients)
-      if(numClients===2){
-       // console.log("full")
-        socket.emit('full', userId);
-      }
-      else{
-        
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId);
-        //console.log("join")
-        // messages
-        socket.on('message', (message,userId) => {
-          //send message to the same room
-          io.to(roomId).emit('createMessage', message,userId)
-      }); 
-    }
-  });
+    socket.on('join-room', (roomId, userId) => {
+        var numClients
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
+        io.of('/').in(roomId).clients(function(error, clients) {
+            numClients = clients.length;
+            // console.log(numClients)
+            if (numClients === 2) {
+                // console.log("full")
+                socket.emit('full', userId);
+            } else {
 
-    socket.on('startcount',(sample)=>{
-      //console.log("call recieve")
-      io.to(roomId).emit('otherstart',userId)
-    })
+                socket.join(roomId)
+                socket.to(roomId).broadcast.emit('user-connected', userId);
+                //console.log("join")
+                // messages
+                socket.on('message', (message, userId) => {
+                    //send message to the same room
+                    io.to(roomId).emit('createMessage', message, userId)
+                });
+            }
+        });
 
-    socket.on('Result',async(rps,roomId)=>{
-     // console.log(rps)
-     await socket.emit('myB',rps)
-     await  socket.to(roomId).emit('OpponentScore',rps)
-     
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+
+        socket.on('startcount', (sample) => {
+            //console.log("call recieve")
+            io.to(roomId).emit('otherstart', userId)
+        })
+
+        socket.on('Result', async(rps, roomId) => {
+            // console.log(rps)
+            await socket.emit('myB', rps)
+            await socket.to(roomId).emit('OpponentScore', rps)
+
+        })
+        socket.on('sstartcount', (sample) => {
+            //console.log("call recieve")
+
+            io.to(roomId).emit('sotherstart', userId)
+        })
+
+        socket.on('sResult', (rps, roomId) => {
+            // console.log(rps)
+            socket.to(roomId).emit('sOpponentScore', rps)
+        })
     })
-    socket.on('sstartcount',(sample)=>{
-      //console.log("call recieve")
-      
-      io.to(roomId).emit('sotherstart',userId)
-    })
-   
-    socket.on('sResult',(rps,roomId)=>{
-     // console.log(rps)
-      socket.to(roomId).emit('sOpponentScore',rps)
-    })
-  })
 })
 
 
-server.listen(process.env.PORT||3030)
+server.listen(process.env.PORT || 3030)
